42a7e7405efc38ce86f045d7ae9405fdb298d291
kebaSE123 <72313637+kebaSE123@users.noreply.github.com>
2023-09-19 12:21:09 +0200

    date

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("04 October 2023 11:59:00 GMT+01:00");			
+		var endTime = new Date("04 October 2023 11:58:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
